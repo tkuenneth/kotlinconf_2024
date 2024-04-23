066036b0514b72ed4d6387736e596a5fa2e90325
Thomas Künneth <kuennetht@gmail.com>
2024-04-23 15:24:02 +0200

    Implementing single instance logic for Windows and Linux

--- FileAssociationDemo/src/main/kotlin/Main.kt
+++ FileAssociationDemo/src/main/kotlin/Main.kt
@@ -21,11 +21,19 @@ import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import java.awt.Desktop
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.ServerSocket
+import java.net.Socket
+import kotlin.concurrent.thread
+
 
 @OptIn(ExperimentalResourceApi::class, ExperimentalComposeUiApi::class)
 @Composable
 fun App(args: List<String>) {
     val fileNames = remember { args.toMutableStateList() }
+    createServerSocket { filename -> fileNames.add(filename) }
     LaunchedEffect(Unit) {
         with(Desktop.getDesktop()) {
             installOpenFileHandler { event ->
@@ -73,11 +81,39 @@ fun App(args: List<String>) {
 @OptIn(ExperimentalResourceApi::class)
 fun main(args: Array<String>) = application {
     setAppName("File Association Demo")
-    Window(
-        title = stringResource(Res.string.app_name),
-        icon = painterResource(Res.drawable.logo),
-        onCloseRequest = ::exitApplication
-    ) {
-        App(args.toList())
+    if (!sendFiles(args)) {
+        Window(
+            title = stringResource(Res.string.app_name),
+            icon = painterResource(Res.drawable.logo),
+            onCloseRequest = ::exitApplication
+        ) {
+            App(args.toList())
+        }
+    }
+}
+
+fun sendFiles(args: Array<String>): Boolean {
+    return if (args.isNotEmpty()) try {
+        val socket = Socket("localhost", 7777)
+        val out = PrintWriter(socket.getOutputStream(), true)
+        args.forEach { filename ->
+            out.println(filename)
+        }
+        socket.close()
+        true
+    } catch (ignore: Throwable) {
+        false
+    } else false
+}
+
+fun createServerSocket(callback: (String) -> Unit) {
+    thread {
+        val serverSocket = ServerSocket(7777)
+        val clientSocket = serverSocket.accept()
+        val `in` = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+        val out = PrintWriter(clientSocket.getOutputStream(), true)
+        while ((`in`.readLine().also { it?.let { message -> callback(message) } }) != null) {
+            out.println("Server: Hello from server!")
+        }
     }
 }
